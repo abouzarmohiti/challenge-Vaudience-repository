7a54da63b95a2ae43f31569cea94ca381733dade
abouzarmohiti <abouzar.mohiti@yahoo.com>
2021-04-14 11:33:35 +0200

    Custom GlobalExceptionHandler // error handle for @Valid

---
+++ src/main/java/com/Vaudience/challenge/exception/CustomGlobalExceptionHandler.java
@@ -0,0 +1,55 @@
+package com.Vaudience.challenge.exception;
+
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    // error handle for @Valid
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status, WebRequest request) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", status.value());
+
+        //Get all errors
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(x -> x.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        body.put("errors", errors);
+
+        return new ResponseEntity<>(body, headers, status);
+
+    }
+
+    @ExceptionHandler(value = ContactIsExistException.class)
+    public ResponseEntity<Object> handleGenericNotFoundException(ContactIsExistException e) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", new Date());
+        body.put("status", HttpStatus.BAD_REQUEST);
+        body.put("errors", e.getMessage());
+        return new ResponseEntity<>(body, null, HttpStatus.BAD_REQUEST);
+
+    }
+
+}
