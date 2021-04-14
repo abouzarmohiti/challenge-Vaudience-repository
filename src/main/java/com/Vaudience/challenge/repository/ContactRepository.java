106797fc9f436862f9dabb8080c6fd5be8df8a1e
abouzarmohiti <abouzar.mohiti@yahoo.com>
2021-04-14 10:48:35 +0200

    add Method findByAddressPostalCode and existsByFullNameAndBirthDate

--- src/main/java/com/Vaudience/challenge/repository/ContactRepository.java
+++ src/main/java/com/Vaudience/challenge/repository/ContactRepository.java
@@ -3,5 +3,14 @@ package com.Vaudience.challenge.repository;
 import com.Vaudience.challenge.model.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public interface ContactRepository extends JpaRepository<Contact, Long> {
+
+    Optional<Contact> findByAddressPostalCode(String postalCode);
+
+    boolean existsByFullNameAndBirthDate(String fullName, LocalDate birthDate);
+
+
 }
